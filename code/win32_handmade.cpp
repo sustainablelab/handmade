baccf3f63850420ee0d22bc9321605b6d3278b5a
sustainablelab <sustainablelab@gmail.com>
2021-02-13 22:26:31 -0500

    HH d003 allocate BitmapMemory

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -7,14 +7,66 @@
 
 #include <windows.h>
 
+#define internal static
 #define local_persist static
 #define global_variable static
 
 // TODO(sustainablelab): This is a global for now.
 global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+
+// DIB -- Device Independent Bitmap
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+
+    // TODO(sustainablelab): Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    if (!BitmapDeviceContext)
+    {
+        // TODO(sustainablelab): Should we recreate these under special circumstances
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+            BitmapDeviceContext,
+            &BitmapInfo,
+            DIB_RGB_COLORS,
+            &BitmapMemory, // <---- THIS IS IT
+            0, 0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    // Copy from buffer to Window
+    StretchDIBits(DeviceContext,
+            X, Y, Width, Height, // Dest - blit to
+            X, Y, Width, Height, // Src - blit from
+            BitmapMemory,
+            &BitmapInfo,
+            DIB_RGB_COLORS, SRCCOPY
+            );
+}
 
 LRESULT CALLBACK
-MainWindowCallback( // "Window Procedure" that lpfnWndProc points to
+Win32MainWindowCallback( // "Window Procedure" that lpfnWndProc points to
     HWND Window,    // Handle to the Window
     UINT Message,   // "Window Notification" from Windows OS
     WPARAM wParam,  // Message-dependent data
@@ -33,6 +85,12 @@ MainWindowCallback( // "Window Procedure" that lpfnWndProc points to
     {
         case WM_SIZE: // https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-size
         {
+            // ClientRect is the part of the window I can draw into.
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width,Height);
             OutputDebugStringA("WM_SIZE\n");
         } break;
 
@@ -58,20 +116,11 @@ MainWindowCallback( // "Window Procedure" that lpfnWndProc points to
         {
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
-            int x = Paint.rcPaint.left;
-            int y = Paint.rcPaint.top;
-            int w = Paint.rcPaint.right - Paint.rcPaint.left;
-            int h = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            local_persist DWORD Operation = BLACKNESS;
-            PatBlt(DeviceContext, x, y, w, h, Operation);
-            if (Operation == BLACKNESS)
-            {
-                Operation = WHITENESS;
-            }
-            else
-            {
-                Operation = BLACKNESS;
-            }
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
         default:
@@ -98,7 +147,7 @@ WinMain(
 
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
     // Pointer to func that handles windows notifications
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
